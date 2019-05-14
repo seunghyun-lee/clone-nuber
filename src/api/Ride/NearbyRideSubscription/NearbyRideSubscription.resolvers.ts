6e6323e231e34c4aa64b9f75f7937acfcd86b881
seunghyun <seunghyunleekr@gmail.com>
2019-05-14 15:28:19 +0900

    bug fix

--- src/api/Ride/NearbyRideSubscription/NearbyRideSubscription.resolvers.ts
+++ src/api/Ride/NearbyRideSubscription/NearbyRideSubscription.resolvers.ts
@@ -6,18 +6,18 @@ const resolvers = {
         NearbyRideSubsctiption: {
             subscribe: withFilter(
                 (_, __, { pubSub }) => pubSub.asyncIterator("rideRequest"),
-                (patload, _, { context }) => {
+                (payload, _, { context }) => {
                     const user: User = context.currentUser;
                     const {
                         NearbyRideSubsctiption: { pickUpLat, pickUpLng }
                     } = payload;
                     const { lastLat: userLastLat, lastLng: userLastLng } = user;
-                    return {
+                    return (
                         pickUpLat >= userLastLat - 0.05 &&
                         pickUpLat <= userLastLat + 0.05 &&
                         pickUpLng >= userLastLng - 0.05 &&
                         pickUpLng <= userLastLng + 0.05
-                    };
+                    );
                 }
             )
         }
